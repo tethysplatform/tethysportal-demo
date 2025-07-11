7231315f46b9d150f63f6b72111c054f48f83092
jakeymac <jakeymacaroni@gmail.com>
2025-07-10 23:31:35 -0600

    Removed broken link to tethys dash application

--- apps/tethysdash/reactapp/components/inputs/Custom.js
+++
@@ -1,3 +0,0 @@
-export { AddMapLayer } from "components/inputs/custom/AddMapLayer";
-export { MapExtent } from "components/inputs/custom/MapExtent";
-export { MapDrawing } from "components/inputs/custom/MapDrawing";
--- apps/tethysdash/tethysapp/tethysdash/tests/integrated_tests/__init__.py
+++
@@ -1,7 +0,0 @@
-import os
-
-# NOTE: database user given must be a superuser to successfully execute all tests.
-default_connection = "postgresql://postgres:pass@localhost:5435/test_tethysdash"
-TEST_DB_URL = os.environ.get(
-    "TETHYSDASH_TEST_DATABASE", default_connection
-)  # noqa: F401, F403
