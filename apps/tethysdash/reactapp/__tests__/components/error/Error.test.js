ce7a1e495bd3c71cb34852d50be4bbfba4d28e76
jakeymac <jakeymacaroni@gmail.com>
2025-07-10 23:31:35 -0600

    Removed broken link to tethys dash application

--- apps/tethysdash/reactapp/__tests__/components/error/Error.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import Error from "components/error/Error";
-
-test("error", async () => {
-  render(<Error title={"Error Title"} image={"some_image"} />);
-
-  expect(await screen.findByText("Reload App")).toBeInTheDocument();
-  expect(await screen.findByText("Exit the App")).toBeInTheDocument();
-});
